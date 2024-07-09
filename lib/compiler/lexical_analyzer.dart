1a989edcc82cbc33351fd5393f966a5a625e8722
mauricio.togneri <mauricio.togneri@gmail.com>
2024-07-09 18:31:08 +0000

    Improvements

--- lib/compiler/lexical_analyzer.dart
+++ lib/compiler/lexical_analyzer.dart
@@ -1,3 +1,4 @@
+import 'package:characters/characters.dart';
 import 'package:dry/models/token.dart';
 
 class LexicalAnalyzer {
@@ -6,6 +7,23 @@ class LexicalAnalyzer {
   const LexicalAnalyzer({required this.source});
 
   List<Token> analyze() {
+    final CharList charList = CharList(list: source.characters.toList());
+
+    while (charList.hasNext) {
+      print(charList.next);
+    }
+
     return [];
   }
+}
+
+class CharList {
+  int index = 0;
+  final List<String> list;
+
+  CharList({required this.list});
+
+  bool get hasNext => index < list.length;
+
+  String get next => list[index++];
 }
