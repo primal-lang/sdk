c6c89a317594539ec59f20e64694b8fbba88f09d
mauricio.togneri <mauricio.togneri@gmail.com>
2024-07-11 21:01:02 +0000

    Improvements

--- lib/compiler/syntactic/expression.dart
+++ lib/compiler/syntactic/expression.dart
@@ -1,16 +1,54 @@
-// string
-// number
-// boolean
-// symbol
-// function_call
-class Expression<T> {
-  final T value;
+class Expression {
+  final ExpressionType type;
 
   const Expression({
+    required this.type,
+  });
+}
+
+class LiteralExpression<T> extends Expression {
+  final T value;
+
+  const LiteralExpression({
+    required super.type,
     required this.value,
   });
 
-  static Expression<String> string(String value) => Expression(
+  static LiteralExpression<String> string(String value) => LiteralExpression(
+        type: ExpressionType.literalString,
         value: value,
       );
+
+  static LiteralExpression<num> number(num value) => LiteralExpression(
+        type: ExpressionType.literalNumber,
+        value: value,
+      );
+
+  static LiteralExpression<bool> boolean(bool value) => LiteralExpression(
+        type: ExpressionType.literalBoolean,
+        value: value,
+      );
+
+  static LiteralExpression<String> symbol(String value) => LiteralExpression(
+        type: ExpressionType.literalSymbol,
+        value: value,
+      );
+}
+
+class FunctionCallExpression extends Expression {
+  final String name;
+  final List<Expression> arguments;
+
+  const FunctionCallExpression({
+    required this.name,
+    required this.arguments,
+  }) : super(type: ExpressionType.functionCall);
+}
+
+enum ExpressionType {
+  literalString,
+  literalNumber,
+  literalBoolean,
+  literalSymbol,
+  functionCall,
 }
