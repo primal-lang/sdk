4521007b4f1fb9a0bc0d0013833c43057459befd
mauricio.togneri <mauricio.togneri@gmail.com>
2024-07-19 18:21:38 +0000

    Improvements

--- test/syntactic_analyzer_test.dart
+++ test/syntactic_analyzer_test.dart
@@ -249,7 +249,7 @@ void main() {
     test('Sample file', () {
       final String source = loadFile('sample.dry');
       final List<FunctionDefinition> functions = getFunctions(source);
-      expect(functions.length, equals(10));
+      expect(functions.length, equals(11));
     });
   });
 }
