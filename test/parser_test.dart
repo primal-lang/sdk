a12b466203025aade34e2fc03ed27b95e1d3902c
mauricio.togneri <mauricio.togneri@gmail.com>
2024-08-20 20:35:13 +0000

    Improvements

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -4,102 +4,83 @@ import 'package:test/test.dart';
 import 'test_utils.dart';
 
 void main() {
+  ParseExpression getExpression(String input) {
+    final List<Token> tokens = getTokens(input);
+    final Parser parser = Parser(tokens);
+
+    return parser.expression();
+  }
+
   group('Parser', () {
     test('Expression 1', () {
-      final List<Token> tokens = getTokens('2 - 1 * 3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('2 - 1 * 3');
       expect(expression.toString(), '(2 - (1 * 3))');
     });
 
     test('Expression 2', () {
-      final List<Token> tokens = getTokens('2 * 1 - 3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('2 * 1 - 3');
       expect(expression.toString(), '((2 * 1) - 3)');
     });
 
     test('Expression 3', () {
-      final List<Token> tokens = getTokens('(2 - 1) * 3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('(2 - 1) * 3');
       expect(expression.toString(), '((2 - 1) * 3)');
     });
 
     test('Expression 4', () {
-      final List<Token> tokens = getTokens('(2 - 1) != 3 * 4');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('(2 - 1) != 3 * 4');
       expect(expression.toString(), '((2 - 1) != (3 * 4))');
     });
 
     test('Expression 5', () {
-      final List<Token> tokens = getTokens('2 * 4 % 3 == 1 + 3 / 4');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression =
+          getExpression('2 * 4 % 3 == 1 + 3 / 4');
       expect(expression.toString(), '(((2 * 4) % 3) == (1 + (3 / 4)))');
     });
 
     test('Expression 6', () {
-      final List<Token> tokens = getTokens('2 + !5 > !7 * 3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('2 + !5 > !7 * 3');
       expect(expression.toString(), '((2 + (!5)) > ((!7) * 3))');
     });
 
     test('Expression 7', () {
-      final List<Token> tokens = getTokens('!2 + 5 >= 7 * !3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('!2 + 5 >= 7 * !3');
       expect(expression.toString(), '(((!2) + 5) >= (7 * (!3)))');
     });
 
     test('Expression 8', () {
-      final List<Token> tokens = getTokens('2 + -5 < -7 * 3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('2 + -5 < -7 * 3');
       expect(expression.toString(), '((2 + (-5)) < ((-7) * 3))');
     });
 
     test('Expression 9', () {
-      final List<Token> tokens = getTokens('-2 + 5 <= 7 * -3');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('-2 + 5 <= 7 * -3');
       expect(expression.toString(), '(((-2) + 5) <= (7 * (-3)))');
     });
 
     test('Expression 10', () {
-      final List<Token> tokens = getTokens('true | 4 < false & "test"');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression =
+          getExpression('true | 4 < false & "test"');
       expect(expression.toString(), '((true | 4) < (false & "test"))');
     });
 
     test('Expression 11', () {
-      final List<Token> tokens = getTokens('foo(4 + 1)');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('foo(4 + 1)');
       expect(expression.toString(), 'foo((4 + 1))');
     });
 
     test('Expression 12', () {
-      final List<Token> tokens = getTokens('foo(bar + 1)');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('foo(bar + 1)');
       expect(expression.toString(), 'foo((bar + 1))');
     });
 
     test('Expression 13', () {
-      final List<Token> tokens = getTokens('foo(bar() + 1)');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('foo(bar() + 1)');
       expect(expression.toString(), 'foo((bar() + 1))');
     });
 
     test('Expression 14', () {
-      final List<Token> tokens = getTokens('foo(bar()() + 1)()');
-      final Parser parser = Parser(tokens);
-      final ParseExpression expression = parser.expression();
+      final ParseExpression expression = getExpression('foo(bar()() + 1)()');
       expect(expression.toString(), 'foo((bar()() + 1))()');
     });
   });
