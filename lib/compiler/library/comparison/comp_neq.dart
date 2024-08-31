026ff2d95347f6eedcde20a5ad4a2a92be78732a
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-08-31 15:01:49 +0200

    Improvements

--- lib/compiler/library/comparison/comp_neq.dart
+++ lib/compiler/library/comparison/comp_neq.dart
@@ -1,4 +1,3 @@
-import 'package:primal/compiler/errors/runtime_error.dart';
 import 'package:primal/compiler/library/comparison/comp_eq.dart';
 import 'package:primal/compiler/models/parameter.dart';
 import 'package:primal/compiler/runtime/reducible.dart';
@@ -19,29 +18,10 @@ class CompNeq extends NativeFunctionPrototype {
   Reducible substitute(Scope<Reducible> arguments) {
     final Reducible a = arguments.get('a').reduce();
     final Reducible b = arguments.get('b').reduce();
+    final Reducible comparison = CompEq().compare(a, b);
 
-    return compare(a, b);
-  }
-
-  Reducible compare(Reducible a, Reducible b) {
-    if ((a is NumberReducibleValue) && (b is NumberReducibleValue)) {
-      return BooleanReducibleValue(a.value != b.value);
-    } else if ((a is StringReducibleValue) && (b is StringReducibleValue)) {
-      return BooleanReducibleValue(a.value != b.value);
-    } else if ((a is BooleanReducibleValue) && (b is BooleanReducibleValue)) {
-      return BooleanReducibleValue(a.value != b.value);
-    } else if ((a is ListReducibleValue) && (b is ListReducibleValue)) {
-      final Reducible comparison = CompEq().compare(a, b);
-
-      return comparison is BooleanReducibleValue
-          ? BooleanReducibleValue(!comparison.value)
-          : comparison;
-    } else {
-      throw InvalidArgumentTypesError(
-        function: name,
-        expected: parameterTypes,
-        actual: [a.type, b.type],
-      );
-    }
+    return comparison is BooleanReducibleValue
+        ? BooleanReducibleValue(!comparison.value)
+        : comparison;
   }
 }
