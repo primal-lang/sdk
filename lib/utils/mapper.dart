d0b85580c9d60fbc164bbd370e896171523afbc9
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-09-07 12:58:24 +0200

    Improvements

--- lib/utils/mapper.dart
+++ lib/utils/mapper.dart
@@ -1,8 +1,7 @@
 import 'package:primal/compiler/runtime/node.dart';
 
 class Mapper {
-  static Map<String, FunctionNode> toMap(
-      List<FunctionNode> functions) {
+  static Map<String, FunctionNode> toMap(List<FunctionNode> functions) {
     final Map<String, FunctionNode> result = {};
 
     for (final FunctionNode function in functions) {
